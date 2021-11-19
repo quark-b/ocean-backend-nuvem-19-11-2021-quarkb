bc03449f4ef2d8ebd3200eee4ab97f0fb9868ca8
Antonio Cintra <antonio.cintra@outlook.com>
2021-11-19 11:46:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,15 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-const dbName = "ocean_bancodados_18_11_2021";
+const url = "mongodb+srv://admin:1b7yo0CUPxyYfFEk@cluster0.749ln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbName = "ocean_nuvem_19_11_2021";
 
 async function main() {
-    /*
     const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection("herois");
-    */
 
-    const collection = undefined;
+    const db = client.db(dbName);
+
+    const collection = db.collection("herois");
 
     const app = express();
 
@@ -99,4 +97,3 @@ main();
 Terms
 Privacy
 Security
-S
